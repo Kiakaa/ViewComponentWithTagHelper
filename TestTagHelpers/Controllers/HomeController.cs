0a932007135ab80f57a7a5c15c455f98f24ae670
Administrator <Administrator@USER-20180625OY>
2018-09-17 16:36:15 +0800

    taghelper is not work in View Component，and View Coponent

---
+++ TestTagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestTagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LoadCopnentViewWithTaghelper()
+        {
+            try
+            {
+                return ViewComponent("Tasks", new { Message = "this view page is from Home/LoadCopnentViewWithTaghelper" });
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception(ex.Message+System.Environment.NewLine+ex.StackTrace);
+            }
+        }
+    }
+}
